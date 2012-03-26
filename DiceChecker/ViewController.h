c346e565ff0ff848f86082ff43f98cf714ae0cf8
Daniel de Haas <ddeha001@ucr.edu>
2012-03-26 15:56:44 -0700

    Initial Commit

---
+++ DiceChecker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiceChecker
+//
+//  Created by  on 3/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
